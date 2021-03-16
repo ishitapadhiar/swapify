09407b83ff280c53e1f7bb8a9f0e6afcfc5597d7
ishitapadhiar <padhii@rpi.edu>
2021-03-16 18:50:14 -0400

    database setup

---
+++ Swapify/Swapify/connection.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['DBUSER'] = 'usxuxwby' 
+os.environ['DBPASS'] = 'sVUyUo4Z1aqH4MiKkLQtWlw8RNMhaq-H'
+os.environ['DBHOST'] = 'queenie.db.elephantsql.com'
+os.environ['DBNAME'] = 'Swapify'
---
+++ Swapify/Swapify/models/artist.py
@@ -0,0 +1,10 @@
+from ..app import db
+
+class Artist(db.Model):
+
+	__tablename__ = 'artist'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String)
+
+	def __init__(self, name=None):
+		self.name = name
