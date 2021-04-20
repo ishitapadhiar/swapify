c6528fd3d8fb3a623e8e3c0d9201fe0a346e573c
ishitapadhiar <padhii@rpi.edu>
2021-04-19 21:08:42 -0400

    commented all models

--- Swapify/Swapify/models/allmodels.py
+++ Swapify/Swapify/models/allmodels.py
@@ -1,36 +1,45 @@
 from ..app import db
 import datetime
 
+
+# Relation table between playlists and songs in the playlist
 playlistsongs = db.Table('playlistsongs',
 	db.Column('playlist_id', db.Integer, db.ForeignKey('playlist.id'), primary_key = True),
 	db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key = True)
 )
 
+# Relation table between two Users that we call friends 
 friends = db.Table('friends',
 	db.Column('user1_id', db.Integer, db.ForeignKey('user.id'), primary_key = True),
 	db.Column('user2_id', db.Integer, db.ForeignKey('user.id'), primary_key = True)
 )
 
+# Relation table be users and the songs that they classify as happy
 happymusiclist = db.Table('happymusiclist',
 	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key = True),
 	db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key = True)
 )
 
+# Relation table be users and the songs that they classify as sad
 sadmusiclist = db.Table('sadmusiclist',
 	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key = True),
 	db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key = True)
 )
 
+# Relation table be users and the songs that they classify as study music
 studymusiclist = db.Table('studymusiclist',
 	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key = True),
 	db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key = True)
 )
 
+# Relation table be users and the songs that they classify as party music
 partymusiclist = db.Table('partymusiclist',
 	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key = True),
 	db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key = True)
 )
 
+
+# This contains the song database model, which has an id, spotify_id, and a song length in minutes
 class Song(db.Model):
 
 	__tablename__ = 'song'
@@ -42,6 +51,9 @@ class Song(db.Model):
 		self.spotify_id = spotify_id
 		self.length = length
 
+# This contains the user model which has the first name, last name,
+# email, spotify_auth, bio, and playlists of the user, and all of their 
+# music classifications
 class User(db.Model):
 
 	__tablename__ = 'user'
@@ -53,6 +65,7 @@ class User(db.Model):
 	bio = db.Column(db.String)
 	playlists = db.relationship('Playlist', backref = 'user', lazy = True)
 
+	# these are connections to the relationship tables for users
 	happy_music = db.relationship('Song', backref='happyusers', lazy='dynamic', secondary=happymusiclist)
 	sad_music = db.relationship('Song', backref='sadusers', lazy='dynamic', secondary=sadmusiclist)
 	study_music = db.relationship('Song', backref='studyusers', lazy='dynamic', secondary=studymusiclist)
@@ -72,6 +85,7 @@ class User(db.Model):
 		self.spotify_auth = spotify_auth
 		self.bio = bio
 
+# The playlist class contains the different playlists that a user might have
 class Playlist(db.Model):
 
 	__tablename__ = 'playlist'
